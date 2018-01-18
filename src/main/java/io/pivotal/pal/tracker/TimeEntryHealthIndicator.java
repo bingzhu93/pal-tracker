063f0fe859be22c92bb530dfa8b51e37c654a59c
weisu00 <weisu00@gmail.com>
2018-01-18 13:48:32 -0700

    added metrics

---
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryHealthIndicator.java
@@ -0,0 +1,24 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TimeEntryHealthIndicator implements HealthIndicator {
+
+    private static final int MAX = 5;
+    private final TimeEntryRepository timeEntryRepo;
+
+    public TimeEntryHealthIndicator(TimeEntryRepository timeEntryRepo) {
+        this.timeEntryRepo = timeEntryRepo;
+    }
+
+    @Override
+    public Health health() {
+        if (timeEntryRepo.list().size() < MAX){
+            return Health.up().build();
+        }
+        return Health.down().build();
+    }
+}
